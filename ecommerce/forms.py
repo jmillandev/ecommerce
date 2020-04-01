278be72427712145918e1f6f9ff3194e94e73921
Jesus Milan <jgmc3012@gmail.com>
2020-04-01 03:34:11 -0400

    :star: feat: Registrar nuevos usuarios

---
+++ ecommerce/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(required=True,
+                                min_length=4, max_length=50,
+                                widget=forms.TextInput(attrs={
+                                    'class': 'form-control',
+                                    'id': 'username',
+                                }))
+    email = forms.EmailField(required=True,
+                                widget=forms.EmailInput(attrs={
+                                    'class': 'form-control',
+                                    'id': 'email',
+                                    'placeholder': 'example@2piksigma.com'
+                                }))
+    password = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control',
+                                }))
+
+    password2 = forms.CharField(label='Confirmar password',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control',
+                                }))
+
+    """
+    Los metodos con el prefijo 'clean_' seguidos del nombre del campo
+    seran ejecutados cuando se ejecute el metodo .is_valid() de la clase.
+    """
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Username no disponible.')
+
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email no disponible.')
+
+        return email
+
+    """
+    Haremos uso de este metodo, si y solo si. Necesitamos validar metodos los cuales
+    dependan uno de otros(Como es el caso de password y password2)
+    """
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'El password no coincide.')
+
+    def save(self):
+        return User.objects.create_user(
+                self.cleaned_data.get('username'),
+                self.cleaned_data.get('email'),
+                self.cleaned_data.get('password'),
+            )
+        
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,6 +5,9 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+from .forms import RegisterForm
 
 def index(request):
     return render(request, 'index.html', {
@@ -34,3 +37,18 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'Sessión cerrada exitosamente. ¡Nos vemos pronto {}!'.format(user.username))
     return redirect('login')
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+
+        user = form.save()
+        if user:
+            login(request, user)
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('index')
+
+    return render(request, 'users/register.html',{
+        'form': form
+    })

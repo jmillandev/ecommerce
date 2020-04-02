b917d643d92f5667c38774d31af94e444190e016
Jesus Milan <jgmc3012@gmail.com>
2020-04-02 03:29:06 -0400

    :recycle: recycle: Modelos Abstracto Para usuarios

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,7 +5,9 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+
+from users.models import User
 
 from .forms import RegisterForm
 
---
+++ users/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+"""
+A continuacion se lista los diferentes atributos que
+podemos utilizar dependiendo de lca clase de la cual
+heredemos.
+
+AbstractUser
+    - username
+    - first_name
+    - last_name
+    - email
+    - password
+    - groups
+    - user_permissions
+    - is_staff
+    - is_active
+    - is_superuser
+    - last_login
+    - data_joined
+
+AbstractBaseUser
+    - id
+    - password
+    - last_login
+"""
+class User(AbstractUser):
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+class Customer(User):
+    """
+    Esta clase agrega funcionabilidad al Modelo User
+    mas no genera una tabla adicional
+    """
+    class Meta:
+        proxy = True
+    
+    def get_products(self):
+        return list()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
